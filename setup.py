224c314aefed311494990efe421f68bcd1e64b56
Itxaka <igarcia@suse.com>
2018-10-08 13:59:10 +0200

    bump release version for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="yarma",
-    version="0.1.0",
+    version="1.0.0",
     description="Yet Another Rabbit Monitoring Agent",
     author="Itxaka Serrano Garcia",
     author_email="igarcia@suse.com",
